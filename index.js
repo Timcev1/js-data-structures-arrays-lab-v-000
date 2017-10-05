5b65d07e55cf9c17a95b55a8f73795105412910f
Timothy Cevallos <timcev@gmail.com>
2017-10-05 21:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"]
+const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name){
-  drivers.push(name)
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name){
-  drivers.unshift(name)
+  drivers.unshift(name);
 }
 
 function destructivelyRemoveLastDriver(){
