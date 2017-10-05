24fc43d9c282cf4abcc6b29958803310577d3c24
Timothy Cevallos <timcev@gmail.com>
2017-10-05 21:26:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function destructivelyRemoveLastDriver(){
 function destructivelyRemoveFirstDriver () {
   drivers.shift();
 }
+function appendDriver (name) {
+  return [...drivers, name];
+}
+
+function prependDriver (name) {
+  return [name, ...drivers];
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
